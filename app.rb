f2d5eba6afaea53b230c3a9f20d8839d536e1114
Rob Knight <raknight@ucsc.edu>
2019-02-28 16:22:40 -0800

    Remove calendar.ucsc.edu from redirects hash

--- app.rb
+++ app.rb
@@ -6,13 +6,12 @@ module REDIRECTOR
   class App < Sinatra::Base
   
     set :redirects, {
-      "artslectures.ucsc.edu" => "https://events.ucsc.edu",
-      "calendar.ucsc.edu" => "https://events.ucsc.edu",      
+      "artslectures.ucsc.edu" => "https://calendar.ucsc.edu",
       "community.ucsc.edu" => "https://connect.ucsc.edu",
       "podcasts.ucsc.edu" => "https://soundcloud.com/ucsantacruz",
       "stayconnected.ucsc.edu" => "https://alumni.ucsc.edu/stay-connected/",
       "urhelp.ucsc.edu" => "https://urishelp.atlassian.net/servicedesk/customer/portals",
-      "urtoolkit.ucsc.edu" => "https://sites.google.com/a/ucsc.edu/ur-toolkit/home?pli=1"
+      "urtoolkit.ucsc.edu" => "https://sites.google.com/a/ucsc.edu/ur-toolkit/home"
     }
     
     tracker = Staccato.tracker(ENV['GOOGLE_ANALYTICS_ID'], nil, ssl: true)
