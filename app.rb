1400f4d1cb2363ef25509b1015d64385a9e8fae7
Rob Knight <raknight@ucsc.edu>
2019-06-04 15:14:21 -0700

    Add science.ucsc.edu

--- app.rb
+++ app.rb
@@ -14,7 +14,8 @@ module REDIRECTOR
       "urtoolkit.ucsc.edu" => "https://sites.google.com/a/ucsc.edu/ur-toolkit/home",
       "maps-gis.ucsc.edu" => "https://maps.ucsc.edu",
       "www.maps.ucsc.edu" => "https://maps.ucsc.edu",
-      "leadership.ucsc.edu" => "https://chancellorsearch.ucsc.edu"
+      "leadership.ucsc.edu" => "https://chancellorsearch.ucsc.edu",
+      "science.ucsc.edu" => "https://dev-ucsc-pbsci.pantheonsite.io"
     }
     
     tracker = Staccato.tracker(ENV['GOOGLE_ANALYTICS_ID'], nil, ssl: true)
