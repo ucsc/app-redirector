c1df4ededf74317e02d1963ee5b0a7036daaab1d
Rob Knight <me@robknight.net>
2019-12-05 07:29:08 -0800

    Remove science.ucsc.edu

--- app.rb
+++ app.rb
@@ -15,7 +15,6 @@ module REDIRECTOR
       "maps-gis.ucsc.edu" => "https://maps.ucsc.edu",
       "www.maps.ucsc.edu" => "https://maps.ucsc.edu",
       "leadership.ucsc.edu" => "https://chancellor.ucsc.edu",
-      "science.ucsc.edu" => "https://dev-ucsc-pbsci.pantheonsite.io",
       "alumniweekend.ucsc.edu" => "https://specialevents.ucsc.edu/alumniweekend"
     }
     
