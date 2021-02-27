8fb3eb52564419a4114fc7c84a3d67769f281cb0
Rob Knight <raknight@ucsc.edu>
2021-02-26 17:05:59 -0800

    Only append path if the request contains one

--- app.rb
+++ app.rb
@@ -41,7 +41,11 @@ module REDIRECTOR
       url = request.env["SERVER_NAME"]
       if settings.redirects[url]
         tracker.event(category: 'App', action: 'Redirect', label: url, value: 1)
-        redirect settings.redirects[url] + request.env["PATH_INFO"]
+        if request.env["PATH_INFO"] != '/'
+          redirect settings.redirects[url] + request.env["PATH_INFO"]
+        else
+          redirect settings.redirects[url]
+        end
       else
         tracker.event(category: 'App', action: 'Not found', label: url, value: 0)
         @host_name = request.env["SERVER_NAME"]
